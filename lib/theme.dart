c7fbfd441e8089ac5529df8dd5f86269b4ef0e98
mohamed <msmlbm30@gmail.com>
2019-10-07 17:28:45 +0200

    fix login with CircularProgress

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Themes {
   static ThemeData kDefaultTheme = ThemeData(
-      primaryColor: Color(0xff071013),
-      backgroundColor: Color(0xffAAAAAA),
+      primaryColor: Color(0xff832161),
+      backgroundColor: Color(0xffC490D1),
       accentColor: Color(0xffffffff));
 }
